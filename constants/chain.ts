580baf7f3afee8994fa556689ee49a00c4ad2ea6
graykode <nlkey2022@gmail.com>
2024-03-18 10:03:00 +0900

    feat: add arbitrum sepolia

--- constants/chain.ts
+++ constants/chain.ts
@@ -1,19 +1,31 @@
+import { arbitrumSepolia } from 'viem/chains'
+
 import { Chain } from '../model/chain'
 
 import { beraTestnetChain } from './dev-chain'
 
-export const supportChains: Chain[] = [
-  {
-    ...beraTestnetChain,
-    defaultGasPrice: 0n,
-    expireIn: 240,
-    icon: '/bera-chain-logo.svg',
-  },
-]
+export const supportChains: Chain[] =
+  process.env.BUILD === 'dev'
+    ? [
+        {
+          ...arbitrumSepolia,
+          defaultGasPrice: 0n,
+          expireIn: 240,
+        },
+      ]
+    : [
+        {
+          ...beraTestnetChain,
+          defaultGasPrice: 0n,
+          expireIn: 240,
+          icon: '/bera-chain-logo.svg',
+        },
+      ]
 
 export const findSupportChain = (chainId: number): Chain | undefined =>
   supportChains.find((chain) => chain.id === chainId)
 
 export enum CHAIN_IDS {
-  BERA_CHAIN_TESTNET = 80085,
+  ARBITRUM_SEPOLIA = arbitrumSepolia.id,
+  BERA_CHAIN_TESTNET = beraTestnetChain.id,
 }
--- constants/subgraph-url.ts
+++ constants/subgraph-url.ts
@@ -3,6 +3,7 @@ import { CHAIN_IDS } from './chain'
 export const SUBGRAPH_URL: {
   [chain in CHAIN_IDS]: string
 } = {
+  [CHAIN_IDS.ARBITRUM_SEPOLIA]: '',
   [CHAIN_IDS.BERA_CHAIN_TESTNET]:
     'https://api.goldsky.com/api/public/project_clsljw95chutg01w45cio46j0/subgraphs/core-v1-subgraph/1.0.0/gn',
 }
