d35119267d9e59515f87a581c487f136300b506c
graykode <nlkey2022@gmail.com>
2024-03-20 10:57:51 +0900

    feat: use readContract

--- utils/unit.ts
+++ utils/unit.ts
@@ -12,18 +12,11 @@ export const calculateUnit = async (chainId: CHAIN_IDS, quote: Currency) => {
     chain: supportChains.find((chain) => chain.id === chainId),
     transport: http(),
   })
-  const [{ result: totalSupply }] = await publicClient.multicall({
-    contracts: [
-      {
-        address: quote.address,
-        abi: ERC20_PERMIT_ABI,
-        functionName: 'totalSupply',
-      },
-    ],
+  const totalSupply = await publicClient.readContract({
+    address: quote.address,
+    abi: ERC20_PERMIT_ABI,
+    functionName: 'totalSupply',
   })
-  if (!totalSupply) {
-    throw new Error('totalSupply is not found')
-  }
   return totalSupply <= 2n ** 64n
     ? 1n
     : 10n ** BigInt(Math.max(quote.decimals - 6, 0))
