c44243f6690df58df7acc8668f56aefe1e51897a
graykode <nlkey2022@gmail.com>
2024-03-20 17:16:59 +0900

    feat: spend logic

---
+++ __tests__/spend.ts
@@ -0,0 +1,181 @@
+import hre from 'hardhat'
+import { zeroAddress, zeroHash } from 'viem'
+import { Contract } from '@ethersproject/contracts'
+
+import { BOOK_VIEWER_ABI } from '../abis/core/book-viewer'
+import { toId } from '../utils/book-id'
+import { BookKey } from '../model/book-key'
+import { encodeToFeePolicy } from '../utils/fee'
+import { MAKER_DEFAULT_POLICY, TAKER_DEFAULT_POLICY } from '../constants/fee'
+import { MAX_TICK, quoteToBase, toPrice } from '../utils/tick'
+import { Book } from '../model/book'
+import { Depth } from '../model/depth'
+
+describe('Spend Logic', () => {
+  const setUp = async ({ blockNumber }: { blockNumber: number }) => {
+    await hre.network.provider.request({
+      method: 'hardhat_reset',
+      params: [
+        {
+          forking: {
+            jsonRpcUrl: 'https://arbitrum-sepolia-archive.allthatnode.com',
+            blockNumber,
+          },
+        },
+      ],
+    })
+    hre.network.provider.emit('hardhatNetworkReset')
+
+    const [admin] = await hre.ethers.getSigners()
+
+    return {
+      admin,
+      BookViewer: await hre.ethers.getContractAt(
+        BOOK_VIEWER_ABI as any,
+        '0x56319f390C3B85Fb8eb18B03b8E14440F3a8c66b',
+        admin,
+      ),
+    }
+  }
+
+  const checkSpendLogic = async ({
+    BookViewer,
+    key,
+    baseAmount,
+  }: {
+    BookViewer: Contract
+    key: BookKey
+    baseAmount: bigint
+  }) => {
+    const actual = await BookViewer.getExpectedOutput({
+      id: toId(key),
+      limitPrice: MAX_TICK,
+      baseAmount,
+      hookData: zeroHash,
+    })
+    const liquidities = await BookViewer.getLiquidity(toId(key), MAX_TICK, 10n)
+    const mockBook = new Book({
+      base: { address: key.base, name: 'mock', symbol: 'mock', decimals: 18 },
+      unit: key.unit,
+      quote: { address: key.quote, name: 'mock', symbol: 'mock', decimals: 18 },
+      makerPolicy: BigInt(key.makerPolicy),
+      hooks: key.hooks,
+      takerPolicy: BigInt(key.takerPolicy),
+      latestTick: 0n,
+      latestPrice: 0n,
+      depths: liquidities
+        .filter((liquidity: any) => BigInt(liquidity.depth) > 0n)
+        .map(
+          (liquidity: any) =>
+            ({
+              bookId: zeroAddress,
+              tick: BigInt(liquidity.tick),
+              price: toPrice(BigInt(liquidity.tick)),
+              rawAmount: BigInt(liquidity.depth),
+              quoteAmount: BigInt(liquidity.depth) * key.unit,
+              baseAmount: quoteToBase(
+                BigInt(liquidity.tick),
+                BigInt(liquidity.depth) * key.unit,
+                true,
+              ),
+            }) as Depth,
+        ),
+    })
+    const expected = mockBook.spend({
+      limitPrice: MAX_TICK,
+      amountIn: baseAmount,
+    })
+    expect(actual.takenQuoteAmount.toString()).toEqual(
+      expected.takenQuoteAmount.toString(),
+    )
+    expect(actual.spendBaseAmount.toString()).toEqual(
+      expected.spendBaseAmount.toString(),
+    )
+  }
+
+  it('spend bid side', async () => {
+    const { BookViewer } = await setUp({ blockNumber: 25020165 })
+    const key: BookKey = {
+      base: zeroAddress,
+      unit: 1n,
+      quote: '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+      makerPolicy: encodeToFeePolicy(true, MAKER_DEFAULT_POLICY),
+      hooks: zeroAddress,
+      takerPolicy: encodeToFeePolicy(true, TAKER_DEFAULT_POLICY),
+    }
+
+    await checkSpendLogic({
+      BookViewer,
+      key,
+      baseAmount: 1n * 10n ** 18n,
+    })
+    await checkSpendLogic({
+      BookViewer,
+      key,
+      baseAmount: 10n * 10n ** 18n,
+    })
+    await checkSpendLogic({
+      BookViewer,
+      key,
+      baseAmount: 0n * 10n ** 18n,
+    })
+    await checkSpendLogic({
+      BookViewer,
+      key,
+      baseAmount: 1n * 10n ** 18n,
+    })
+    await checkSpendLogic({
+      BookViewer,
+      key,
+      baseAmount: 5n * 10n ** 17n,
+    })
+    await checkSpendLogic({
+      BookViewer,
+      key,
+      baseAmount: 1234n * 10n ** 14n,
+    })
+  }, 100000)
+
+  it('spend ask side', async () => {
+    const { BookViewer } = await setUp({ blockNumber: 25036851 })
+    const key: BookKey = {
+      quote: zeroAddress,
+      unit: 10n ** 12n,
+      base: '0x00bfd44e79fb7f6dd5887a9426c8ef85a0cd23e0',
+      makerPolicy: encodeToFeePolicy(true, MAKER_DEFAULT_POLICY),
+      hooks: zeroAddress,
+      takerPolicy: encodeToFeePolicy(true, TAKER_DEFAULT_POLICY),
+    }
+
+    await checkSpendLogic({
+      BookViewer,
+      key,
+      baseAmount: 1n * 10n ** 18n,
+    })
+    await checkSpendLogic({
+      BookViewer,
+      key,
+      baseAmount: 10n * 10n ** 18n,
+    })
+    await checkSpendLogic({
+      BookViewer,
+      key,
+      baseAmount: 0n * 10n ** 18n,
+    })
+    await checkSpendLogic({
+      BookViewer,
+      key,
+      baseAmount: 1n * 10n ** 18n,
+    })
+    await checkSpendLogic({
+      BookViewer,
+      key,
+      baseAmount: 5n * 10n ** 17n,
+    })
+    await checkSpendLogic({
+      BookViewer,
+      key,
+      baseAmount: 1234n * 10n ** 14n,
+    })
+  }, 100000)
+})
--- model/book.ts
+++ model/book.ts
@@ -1,4 +1,4 @@
-import { divide, quoteToBase, toPrice } from '../utils/tick'
+import { baseToQuote, divide, quoteToBase, toPrice } from '../utils/tick'
 import {
   calculateFee,
   calculateOriginalAmount,
@@ -60,6 +60,12 @@ export class Book {
   }) => {
     let takenQuoteAmount = 0n
     let spendBaseAmount = 0n
+    if (this.depths.length === 0) {
+      return {
+        takenQuoteAmount,
+        spendBaseAmount,
+      }
+    }
 
     const ticks = this.depths
       .sort((a, b) => Number(b.price) - Number(a.price))
@@ -113,5 +119,68 @@ export class Book {
     }
   }
 
-  // spend function
+  spend = ({
+    limitPrice,
+    amountIn, // base
+  }: {
+    limitPrice: bigint
+    amountIn: bigint
+  }) => {
+    let takenQuoteAmount = 0n
+    let spendBaseAmount = 0n
+    if (this.depths.length === 0) {
+      return {
+        takenQuoteAmount,
+        spendBaseAmount,
+      }
+    }
+
+    const ticks = this.depths
+      .sort((a, b) => Number(b.price) - Number(a.price))
+      .map((depth) => depth.tick)
+    let index = 0
+    let tick = ticks[index]
+    while (spendBaseAmount <= amountIn && tick > -8388608n) {
+      if (limitPrice > toPrice(tick)) {
+        break
+      }
+      let maxAmount = getUsesQuote(this.takerPolicy)
+        ? amountIn - spendBaseAmount
+        : calculateOriginalAmount(
+            this.takerPolicy,
+            amountIn - spendBaseAmount,
+            false,
+          )
+      maxAmount = baseToQuote(tick, maxAmount, false) / this.unit
+
+      if (maxAmount === 0n) {
+        break
+      }
+      const currentDepth = this.depths.find((depth) => depth.tick === tick)!
+      let quoteAmount =
+        (currentDepth.rawAmount > maxAmount
+          ? maxAmount
+          : currentDepth.rawAmount) * this.unit
+      let baseAmount = quoteToBase(tick, quoteAmount, true)
+      if (getUsesQuote(this.takerPolicy)) {
+        quoteAmount =
+          quoteAmount - calculateFee(this.takerPolicy, quoteAmount, false)
+      } else {
+        baseAmount =
+          baseAmount + calculateFee(this.takerPolicy, baseAmount, false)
+      }
+      if (baseAmount === 0n) {
+        break
+      }
+
+      takenQuoteAmount += quoteAmount
+      spendBaseAmount += baseAmount
+      index++
+      tick = ticks[index]
+    }
+    return {
+      takenQuoteAmount,
+      spendBaseAmount,
+    }
+  }
 }
