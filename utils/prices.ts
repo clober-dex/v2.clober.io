2d5a2a090711f16cc2d2d2490a17103bc5c52415
graykode <nlkey2022@gmail.com>
2024-05-21 23:09:27 +0900

    fix: `r`

--- utils/prices.ts
+++ utils/prices.ts
@@ -2,7 +2,7 @@ import BigNumber from 'bignumber.js'
 
 import { getDecimalPlaces } from './bignumber'
 
-export const getPriceDecimals = (price: number, r: number = 1.001) => {
+export const getPriceDecimals = (price: number, r: number = 1.0001) => {
   const priceNumber = new BigNumber(price)
   return getDecimalPlaces(
     new BigNumber(r).multipliedBy(priceNumber).minus(priceNumber),
