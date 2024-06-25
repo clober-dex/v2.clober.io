dabb1809dbb4b49aff38c9287cb911f7b5fd3cff
graykode <nlkey2022@gmail.com>
2024-06-25 17:02:38 +0900

    fix: `getDecimalPlaces` function

--- utils/prices.ts
+++ utils/prices.ts
@@ -1,11 +1,11 @@
 import BigNumber from 'bignumber.js'
 
-import { getDecimalPlaces } from './bignumber'
+import { findFirstNonZeroIndex } from './bignumber'
 
 export const getPriceDecimals = (price: number, r: number = 1.0001) => {
   const priceNumber = new BigNumber(price)
-  return getDecimalPlaces(
-    new BigNumber(r).multipliedBy(priceNumber).minus(priceNumber),
-    1,
-  )
+  const priceDiff = new BigNumber(r)
+    .multipliedBy(priceNumber)
+    .minus(priceNumber)
+  return findFirstNonZeroIndex(priceDiff)
 }
