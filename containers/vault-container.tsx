e754a185b7f295fd7a0ff12aa6d47c62f5dae96d
graykode <nlkey2022@gmail.com>
2024-07-05 16:40:12 +0900

    feat: vault upside

--- containers/vault-container.tsx
+++ containers/vault-container.tsx
@@ -1,19 +1,95 @@
 import React from 'react'
+import { useAccount } from 'wagmi'
 
 export const VaultContainer = () => {
+  const { address: userAddress } = useAccount()
+
+  const [tab, setTab] = React.useState<'my-liquidity' | 'vault'>('vault')
+
   return (
     <div className="w-full flex flex-col text-white">
-      <div className="flex justify-center w-auto h-[400px] bg-gradient-to-t from-slate-900 to-slate-900">
-        <div className="w-[960px] h-[232px] mt-16 flex flex-col gap-12 items-center">
-          <div className="flex w-[261px] h-[72px] flex-col justify-start items-center gap-3">
-            <div className="self-stretch text-center text-white text-4xl font-bold">
-              Pool
+      <div className="flex justify-center w-auto sm:h-[400px] bg-gradient-to-t from-slate-900 to-slate-900">
+        <div className="w-[960px] mt-8 sm:mt-16 flex flex-col sm:gap-12 items-center">
+          <div className="flex w-[261px] h-12 sm:h-[72px] flex-col justify-start items-center gap-2 sm:gap-3">
+            <div className="self-stretch text-center text-white text-lg sm:text-4xl font-bold">
+              Vault
             </div>
-            <div className="self-stretch text-center text-gray-400 text-sm font-bold">
+            <div className="self-stretch text-center text-gray-400 text-xs sm:text-sm font-bold">
               Provide liquidity and earn fees!
             </div>
           </div>
-          <div>2</div>
+          <div className="flex w-full h-20 mt-6 sm:mt-0 sm:h-28 px-4 justify-start items-center gap-3 sm:gap-4">
+            <div className="grow shrink basis-0 h-full px-6 py-4 sm:px-8 sm:py-6 bg-[rgba(96,165,250,0.10)] rounded-xl sm:rounded-2xl flex-col justify-center items-center gap-3 inline-flex">
+              <div className="text-center text-gray-400 text-sm font-semibold">
+                TVL
+              </div>
+              <div className="self-stretch text-center text-white text-lg sm:text-2xl font-bold">
+                $612,384
+              </div>
+            </div>
+            <div className="grow shrink basis-0 h-full px-6 py-4 sm:px-8 sm:py-6 bg-[rgba(96,165,250,0.10)] rounded-xl sm:rounded-2xl flex-col justify-center items-center gap-3 inline-flex">
+              <div className="text-center text-gray-400 text-sm font-semibold">
+                24h Volume
+              </div>
+              <div className="self-stretch text-center text-white text-lg sm:text-2xl font-bold">
+                $31,582
+              </div>
+            </div>
+          </div>
+          <div className="flex w-full mt-8 sm:mt-0 sm:mr-auto px-4">
+            <div className="w-full sm:w-[328px] h-[40px] sm:h-[56px] items-center flex">
+              <button
+                onClick={() => setTab('vault')}
+                disabled={tab === 'vault'}
+                className="flex flex-1 gap-2 items-center justify-center w-full h-full disabled:bg-gray-950 bg-transparent rounded-tl-2xl rounded-tr-2xl"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  className="w-4 h-4 sm:w-6 sm:h-6"
+                >
+                  <path
+                    d="M2 20C2.22717 20.3165 2.52797 20.5729 2.87642 20.7472C3.22488 20.9214 3.6105 21.0082 4 21C4.3895 21.0082 4.77512 20.9214 5.12358 20.7472C5.47203 20.5729 5.77283 20.3165 6 20C6.22717 19.6835 6.52797 19.4271 6.87642 19.2528C7.22488 19.0786 7.6105 18.9918 8 19C8.3895 18.9918 8.77512 19.0786 9.12358 19.2528C9.47203 19.4271 9.77283 19.6835 10 20C10.2272 20.3165 10.528 20.5729 10.8764 20.7472C11.2249 20.9214 11.6105 21.0082 12 21C12.3895 21.0082 12.7751 20.9214 13.1236 20.7472C13.472 20.5729 13.7728 20.3165 14 20C14.2272 19.6835 14.528 19.4271 14.8764 19.2528C15.2249 19.0786 15.6105 18.9918 16 19C16.3895 18.9918 16.7751 19.0786 17.1236 19.2528C17.472 19.4271 17.7728 19.6835 18 20C18.2272 20.3165 18.528 20.5729 18.8764 20.7472C19.2249 20.9214 19.6105 21.0082 20 21C20.3895 21.0082 20.7751 20.9214 21.1236 20.7472C21.472 20.5729 21.7728 20.3165 22 20M2 16C2.22717 16.3165 2.52797 16.5729 2.87642 16.7472C3.22488 16.9214 3.6105 17.0082 4 17C4.3895 17.0082 4.77512 16.9214 5.12358 16.7472C5.47203 16.5729 5.77283 16.3165 6 16C6.22717 15.6835 6.52797 15.4271 6.87642 15.2528C7.22488 15.0786 7.6105 14.9918 8 15C8.3895 14.9918 8.77512 15.0786 9.12358 15.2528C9.47203 15.4271 9.77283 15.6835 10 16C10.2272 16.3165 10.528 16.5729 10.8764 16.7472C11.2249 16.9214 11.6105 17.0082 12 17C12.3895 17.0082 12.7751 16.9214 13.1236 16.7472C13.472 16.5729 13.7728 16.3165 14 16C14.2272 15.6835 14.528 15.4271 14.8764 15.2528C15.2249 15.0786 15.6105 14.9918 16 15C16.3895 14.9918 16.7751 15.0786 17.1236 15.2528C17.472 15.4271 17.7728 15.6835 18 16C18.2272 16.3165 18.528 16.5729 18.8764 16.7472C19.2249 16.9214 19.6105 17.0082 20 17C20.3895 17.0082 20.7751 16.9214 21.1236 16.7472C21.472 16.5729 21.7728 16.3165 22 16M15 12V4.5C15 4.10218 15.158 3.72064 15.4393 3.43934C15.7206 3.15804 16.1022 3 16.5 3C16.8978 3 17.2794 3.15804 17.5607 3.43934C17.842 3.72064 18 4.10218 18 4.5M9 12V4.5C9 4.10218 8.84196 3.72064 8.56066 3.43934C8.27936 3.15804 7.89782 3 7.5 3C7.10218 3 6.72064 3.15804 6.43934 3.43934C6.15804 3.72064 6 4.10218 6 4.5M15 5H9M9 10H15"
+                    stroke="white"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+                <div className="text-center text-white text-sm sm:text-base font-bold">
+                  Vault
+                </div>
+              </button>
+              <button
+                onClick={() => userAddress && setTab('my-liquidity')}
+                disabled={tab === 'my-liquidity'}
+                className="flex flex-1 gap-2 items-center justify-center w-full h-full disabled:bg-gray-950 bg-transparent rounded-tl-2xl rounded-tr-2xl"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  className="w-4 h-4 sm:w-6 sm:h-6"
+                >
+                  <path
+                    d="M11.9998 17.75L5.82784 20.995L7.00684 14.122L2.00684 9.25495L8.90684 8.25495L11.9928 2.00195L15.0788 8.25495L21.9788 9.25495L16.9788 14.122L18.1578 20.995L11.9998 17.75Z"
+                    stroke="#6B7280"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+                <div className="text-center text-gray-500 text-sm sm:text-base font-bold">
+                  My Liquidity
+                </div>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex">Down</div>
