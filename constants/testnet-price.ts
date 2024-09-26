d3c26d7a096f4497b97f965f2b2158a2802b44b8
graykode <nlkey2022@gmail.com>
2024-09-26 14:07:23 +0900

    feat: show dummy price for testnet

---
+++ constants/testnet-price.ts
@@ -0,0 +1,13 @@
+import { CHAIN_IDS } from '@clober/v2-sdk'
+
+import { Prices } from '../model/prices'
+
+export const TESTNET_PRICES: {
+  [chain in CHAIN_IDS]: Prices
+} = {
+  [CHAIN_IDS.ARBITRUM_SEPOLIA]: {
+    '0x00BFD44e79FB7f6dd5887A9426c8EF85A0CD23e0': 1,
+    '0xF2e615A933825De4B39b497f6e6991418Fb31b78': 2500,
+  } as Prices,
+  [CHAIN_IDS.BERACHAIN_TESTNET]: {} as Prices,
+}
--- contexts/currency-context.tsx
+++ contexts/currency-context.tsx
@@ -12,6 +12,8 @@ import { ERC20_PERMIT_ABI } from '../abis/@openzeppelin/erc20-permit-abi'
 import { fetchPrices } from '../apis/swap/prices'
 import { AGGREGATORS } from '../constants/aggregators'
 import { Allowances } from '../model/allowances'
+import { testnetChainIds } from '../constants/chain'
+import { TESTNET_PRICES } from '../constants/testnet-price'
 
 import { useChainContext } from './chain-context'
 
@@ -125,6 +127,9 @@ export const CurrencyProvider = ({ children }: React.PropsWithChildren<{}>) => {
   const { data: prices } = useQuery(
     ['prices', selectedChain],
     async () => {
+      if (testnetChainIds.includes(selectedChain.id)) {
+        return TESTNET_PRICES[selectedChain.id] as Prices
+      }
       return fetchPrices(AGGREGATORS[selectedChain.id])
     },
     {
