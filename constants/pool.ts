8d3330b2f9eb0ee4e6b078a0375f7d3f67fb1a3d
graykode <nlkey2022@gmail.com>
2024-10-25 16:03:09 +0900

    feat: add base default vault

--- constants/pool.ts
+++ constants/pool.ts
@@ -18,6 +18,13 @@ export const POOL_KEY_INFOS: {
     },
   ],
   [CHAIN_IDS.BERACHAIN_TESTNET]: [],
-  [CHAIN_IDS.BASE]: [],
+  [CHAIN_IDS.BASE]: [
+    {
+      token0: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
+      token1: '0x4200000000000000000000000000000000000006',
+      salt: zeroHash,
+      key: '0xc8cbe608c82ee9c4c30f01d7c0eefd977538ac396ed34430aa3993bfe0d363ae',
+    },
+  ],
   [CHAIN_IDS.ZKSYNC]: [],
 }
